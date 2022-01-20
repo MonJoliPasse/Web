75813c1b14878c6cfba898eea04e1dc0824ca569
MonJoliPasse <98060646+MonJoliPasse@users.noreply.github.com>
2022-01-20 09:52:08 +0100

    add analytics

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+const VueAnalytics = require('vue-analytics').default
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(VueAnalytics, {
+  id: 'UA-217737194-1'
+})
+app.mount('#app')
