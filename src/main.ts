dbca10e3c6f11a276c7ab1d228b717ddf8f19601
MonJoliPasse <98060646+MonJoliPasse@users.noreply.github.com>
2022-01-20 09:58:01 +0100

    Revert "add analytics"
    This reverts commit 75813c1b14878c6cfba898eea04e1dc0824ca569.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-const VueAnalytics = require('vue-analytics').default
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 
-const app = createApp(App)
-app.use(VueAnalytics, {
-  id: 'UA-217737194-1'
-})
-app.mount('#app')
+createApp(App).mount('#app')
