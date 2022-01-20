0785b6fefaff8a307e330be654300034473beb83
MonJoliPasse <98060646+MonJoliPasse@users.noreply.github.com>
2022-01-20 10:01:18 +0100

    add ua-tag

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+const VueGtag = require('vue-gtag').default
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(VueGtag,{ config :   {
+  id: 'UA-217737194-1'
+}})
+app.mount('#app')
